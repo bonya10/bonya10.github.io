583bc3f56492a180c50cb86b6f83e23efecac18a
bonya10 <36155714+bonya10@users.noreply.github.com>
2018-04-19 11:24:14 +0200

    //light effect nav menu

--- js/main.js
+++ js/main.js
@@ -42,7 +42,17 @@ $(document).ready(function() {
 		}, 500);
 	  return false;
 	});
+//light effect nav menu
 
+Array.from(document.querySelectorAll('.fancy-hover'),function(el){
+
+  el.addEventListener('mousemove',function(e){
+    el.style.setProperty('--px', e.clientX - el.offsetLeft);
+    el.style.setProperty('--py', e.clientY - el.offsetTop);
+  });
+
+});
+	
 	// Mobile Navigation
 	$('.nav-toggle').on('click', function() {
 		$(this).toggleClass('close-nav');
