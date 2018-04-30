3e3d7d3537e4ced536f4564eec4125accfb06d70
lumiere10 <alin4eg10@gmail.com>
2018-04-30 23:31:11 +0300

    2

---
+++ js/map.js
@@ -0,0 +1,35 @@
+ymaps.ready(function ()  {
+    var myMap = new ymaps.Map('map',  {
+            center: [53.670286, 23.836715],
+            zoom: 16
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+
+        // Создаём макет содержимого.
+        MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+            '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+        ),
+
+        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+            hintContent: 'Собственный значок метки',
+            balloonContent: 'Это красивая метка'
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#image',
+            // Своё изображение иконки метки.
+            iconImageHref: '../images/icon_pin.png',
+            // Размеры метки.
+            iconImageSize: [30, 42],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-5, -38]
+        })
+
+       
+    
+   myMap.geoObjects
+        .add(myPlacemark)
+        
+});
