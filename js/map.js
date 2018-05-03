e5bfaca73d09e30e559cfaed392aeb957ff1a661
lumiere10 <36155714+lumiere10@users.noreply.github.com>
2018-05-03 14:34:17 +0300

    Update map.js

--- js/map.js
+++ js/map.js
@@ -11,21 +11,7 @@ ymaps.ready(function ()  {
             '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
         ),
 
-        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
-            hintContent: 'Собственный значок метки',
-            balloonContent: 'Это красивая метка'
-        }, {
-            // Опции.
-            // Необходимо указать данный тип макета.
-            iconLayout: 'default#image',
-            // Своё изображение иконки метки.
-            iconImageHref: '../images/icon_pin.png',
-            // Размеры метки.
-            iconImageSize: [30, 42],
-            // Смещение левого верхнего угла иконки относительно
-            // её "ножки" (точки привязки).
-            iconImageOffset: [-5, -38]
-        })
+       
 
        
     
