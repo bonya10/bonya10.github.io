5836ba0c86a185d57954c078bfcdfec91b19570d
lumiere10 <36155714+lumiere10@users.noreply.github.com>
2018-05-03 14:09:15 +0300

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -20,7 +20,7 @@ $(document).ready(function() {
         e.preventDefault();
         $(this).get(0).reset();
        $('.modal1').hide();
-        $('.submit-success').fadeToggle(fast);
+        $('.submit-success').fadeToggle("fast");
      
    
       }
