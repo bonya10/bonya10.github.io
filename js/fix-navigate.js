22d95b726c0e4e1807c39f8a27353f5739a1de28
lumiere10 <36155714+lumiere10@users.noreply.github.com>
2018-05-04 11:12:02 +0300

    Update fix-navigate.js

--- js/fix-navigate.js
+++ js/fix-navigate.js
@@ -1,3 +1,4 @@
 jQuery(document).ready(function($) {
-    $('.navigation a[href^="/' + location.pathname.split("/")[1] + '"]').addClass('active');
+    $('.navigation a[href^="/' + location.pathname.split("/")[1] + '"]').parent("li").addClass('active');
+    
 });
