057e3f536799cdab1f3849b2881031de855e9f42
lumiere10 <36155714+lumiere10@users.noreply.github.com>
2018-05-10 15:09:17 +0300

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -5,17 +5,17 @@ $(document).ready(function() {
       
         $.ajax({
           method: 'POST',
-          url: 'https://formspree.io/alinka_cydik@mail.ru',
+          url: 'https://formspree.io/alinka_cydik@mail.ru',"https://formspree.io/you@email.com",
           data: $('#contact-form').serialize(),
           datatype: 'json'
-        });
+ success:function() {
         e.preventDefault();
         $(this).get(0).reset();
        $('.modal1').hide();
         $('.submit-success').fadeToggle(100);
-     
+        }
    
-      }
+      }  });
     });
   
   
