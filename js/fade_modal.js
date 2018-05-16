5827b52e08affb9c7c0ed662b7630f7ad7199a2f
lumiere10 <36155714+lumiere10@users.noreply.github.com>
2018-05-16 15:18:47 +0300

    Update fade_modal.js

--- js/fade_modal.js
+++ js/fade_modal.js
@@ -1,9 +1,8 @@
 $(document).ready(function() {
 $(".modal1").fadeToggle('fast');
      });
-       $(".modal1").bind("click",function(e){
--        if($(e.target).attr("class") != "modal-form")
-+        if($(e.target).attr("class") != "text")
-       $(".modal1").fadeOut('fast');
-       })
- });
+     $(".modal1").bind("click",function(e){
+        if($(e.target).attr("class") != "modal-form")
+      $(".modal1").fadeOut('fast');
+      })
+});
