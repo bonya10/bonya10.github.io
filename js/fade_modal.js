2dc57c7332cbe5d629fec596be30f5e0316bc2fb
lumiere10 <36155714+lumiere10@users.noreply.github.com>
2018-05-16 14:24:12 +0300

    Update fade_modal.js

--- js/fade_modal.js
+++ js/fade_modal.js
@@ -7,3 +7,16 @@ $(document).ready(function() {
       $(".modal1").fadeOut('fast');
       })
 });
+
+window.addEventListener('DOMContentLoaded', function() {
+      var page = document.querySelector(".modal1");
+      var shimModalContent = document.querySelector(".shim-modal-content");
+      page.addEventListener("click", function(event){
+          var target = event.target;
+          if(target.closest(".modal-content-window") && !target.closest(".send") && !target.closest(".close"))
+          event.stopPropagation();
+          else if( target.closest(".shim-modal-content") )
+          shimModalContent.classList.remove("shim-modal-show");
+          else shimModalContent.classList.add("shim-modal-show");
+      });
+    });
