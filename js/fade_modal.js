538d9df579df890e28a84e7b1e89e667ba49c96b
lumiere10 <36155714+lumiere10@users.noreply.github.com>
2018-05-16 16:26:56 +0300

    Update fade_modal.js

--- js/fade_modal.js
+++ js/fade_modal.js
@@ -1,1 +1,11 @@
 
+var test = document.querySelector('.modal1');
+
+function removeTest(e) {
+    if(!e.target.matches('.modal1')) {
+        test.remove();
+        this.removeEventListener('click', removeTest);
+    }
+};
+
+window.addEventListener('click', removeTest);
