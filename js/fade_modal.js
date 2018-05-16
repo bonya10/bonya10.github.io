bd5e8cdcae64039cda2c5f7bd6d50a3eea54e9ea
lumiere10 <36155714+lumiere10@users.noreply.github.com>
2018-05-16 16:09:31 +0300

    Update fade_modal.js

--- js/fade_modal.js
+++ js/fade_modal.js
@@ -1,6 +1,6 @@
-window.onload=function(){
+$(document).ready(function(){
   document.body.onclick=function(){
      if(µ) {µ =false; return;}
      document.getElementById('modal1').style.display="none"
   }
-}
+});
