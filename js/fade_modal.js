940960c733665f9b62d1c6c38fc55875524ca7b7
lumiere10 <36155714+lumiere10@users.noreply.github.com>
2018-05-17 10:16:34 +0300

    Update fade_modal.js

--- js/fade_modal.js
+++ js/fade_modal.js
@@ -1,1 +1,12 @@
 
+$('.but').click(function () {
+    $('#modal1').toggleClass('opened');
+});
+$(document).click(function (e) {
+    if (!$(e.target).parents().andSelf().is('.but')) {
+        $("#modal1").removeClass("opened");
+    }
+});
+$("#modal1").click(function (e) {
+    e.stopPropagation();
+});
