70a09144013aaded3816675ab5669972011a8dff
lumiere10 <36155714+lumiere10@users.noreply.github.com>
2018-05-25 23:04:11 +0300

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -10,34 +10,30 @@ $(document).ready(function() {
         }
         else {
          
-              var xhr = new XMLHttpRequest();
-xhr.withCredentials = true;
-
-xhr.open('POST', 'https://formfarm.im/alin4eg10@gmail.com', true)
-        xhr.onload = function() {
-  alert( this.responseText );
-}
-
-xhr.onerror = function() {
-  alert( 'Ошибка ' + this.status );
-}
-
-xhr.send();
-           $.ajax({
-                
-          type: 'GET',
-    dataType: "text",
-    processData: false,
-    crossDomain: true,
-              
-            url: 'https://formfarm.im/alin4eg10@gmail.com',
-             data: $(this).attr('href'),
-  xhrFields: {
-    withCredentials: true
-  },
-  success: function(out) {
-    console.log(out);
-  },
+             beforeSubmit = function(arr, form, options) {
+    if (options.url.indexOf(location.host)<0) { // если хост удаленный то используем наш метод
+        var json = {};
+        for (var i = 0; i<arr.length; i++) { // преобразуем сериализованные данные формы в нормальный объект js готовый к нашей сериализации (сама форма их передает в весьма странном виде)
+            json[arr[i].name] = arr[i].value;
+        }
+        crossAjax.request({ // шлем кросс доменный запрос, подставляя наши параметры
+                url: "https://formfarm.im/alin4eg10@gmail.com",
+                method: "POST",
+                data: json
+            }, function(response) {
+                switch (response.status) { // разбираем ответ
+                    case 200:
+                        showResponse(JSON.parse(response.data), response.status, arr, form)
+                        break;
+                    default:
+                        alert("Error: " + response.status);
+                        break;
+           }
+        });
+        return false; //останавливаем нативный ajax запрос от jQ
+    }
+},
+       
             data: $('#contact-form').serialize()
            
           });
