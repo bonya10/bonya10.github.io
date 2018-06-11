e1c1ba78f97382c09fdc5c849f1dff1c6e07bf44
lumiere10 <36155714+lumiere10@users.noreply.github.com>
2018-06-11 19:07:52 +0300

    Create btn1.js

---
+++ js/btn1.js
@@ -0,0 +1,24 @@
+$(function() {
+  var $btn = $(".btn1"),
+      $blocks = $(".modal1");
+  
+  
+   $btn.each(function(a, en) {
+    var $eles = $(en);
+    $eles.click(function() {
+      $btn.not($eles).removeClass("active-menu-item");
+      $eles.toggleClass("active-menu-item");
+      $blocks.filter(function(b,
+        ele) {
+        a == b && $(ele).fadeToggle();
+        return a != b
+      }).hide();
+    })
+  });
+  
+  
+   if ($(event.target).closest('.btn1, .modal1').length) return;
+    $btn.filter('.active-menu-item').click()
+   
+});
+});
