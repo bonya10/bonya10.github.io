d001943959792bdc3568fbe100481fa236a82c7c
lumiere10 <36155714+lumiere10@users.noreply.github.com>
2018-06-11 19:02:18 +0300

    Update fade_modal.js

--- js/fade_modal.js
+++ js/fade_modal.js
@@ -1,7 +1,5 @@
 $(function() {
   var $but = $(".but"),
-       $btn = $(".btn1"),
-     $btn2 = $(".btn2"),
       $blocks = $(".modal1");
   
   $but.each(function(i, elem) {
@@ -17,41 +15,11 @@ $(function() {
     })
   }); 
   
-   $btn.each(function(a, en) {
-    var $eles = $(en);
-    $eles.click(function() {
-      $btn.not($eles).removeClass("active-menu-item");
-      $eles.toggleClass("active-menu-item");
-      $blocks.filter(function(b,
-        ele) {
-        a == b && $(ele).fadeToggle();
-        return a != b
-      }).hide();
-    })
-  });
-  
-    $btn2.each(function(c, eli) {
-    var $eli = $(eli);
-    $eli.click(function() {
-      $btn2.not($eli).removeClass("active-menu-item");
-      $eli.toggleClass("active-menu-item");
-      $blocks.filter(function(d,
-        ely) {
-        c == d && $(ely).fadeToggle();
-        return c != d
-      }).hide();
-    })
-  });
   
    $('html').click(function(event) {
     if ($(event.target).closest('.but, .modal1').length) return;
     $but.filter('.active-menu-item').click()
-     
-    if ($(event.target).closest('.btn1, .modal1').length) return;
-    $btn.filter('.active-menu-item').click()
-     
-    if ($(event.target).closest('.btn2, .modal1').length) return;
-    $btn2.filter('.active-menu-item').click()
+    
 });
  });
 
