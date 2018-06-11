f526568c72901c1a0f498614f03f57880a7b89e6
lumiere10 <36155714+lumiere10@users.noreply.github.com>
2018-06-11 19:07:23 +0300

    Update fade_modal.js

--- js/fade_modal.js
+++ js/fade_modal.js
@@ -24,50 +24,5 @@ $(function() {
  });
 
 
-$(function() {
-  var $btn = $(".btn1"),
-      $blocks = $(".modal1");
-  
-  
-   $btn.each(function(a, en) {
-    var $eles = $(en);
-    $eles.click(function() {
-      $btn.not($eles).removeClass("active-menu-item");
-      $eles.toggleClass("active-menu-item");
-      $blocks.filter(function(b,
-        ele) {
-        a == b && $(ele).fadeToggle();
-        return a != b
-      }).hide();
-    })
-  });
-  
-  
-   if ($(event.target).closest('.btn1, .modal1').length) return;
-    $btn.filter('.active-menu-item').click()
-   
-});
-});
 
-$(function() {
-  var $btn2 = $(".btn2"),
-      $blocks = $(".modal1");
-  
-     $btn2.each(function(c, eli) {
-    var $eli = $(eli);
-    $eli.click(function() {
-      $btn2.not($eli).removeClass("active-menu-item");
-      $eli.toggleClass("active-menu-item");
-      $blocks.filter(function(d,
-        ely) {
-        c == d && $(ely).fadeToggle();
-        return c != d
-      }).hide();
-    })
-  });
-  
-  
-    if ($(event.target).closest('.btn2, .modal1').length) return;
-    $btn2.filter('.active-menu-item').click()
-});
-});
+
